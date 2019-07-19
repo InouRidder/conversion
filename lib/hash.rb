b3128d16c46c1d9c39b4d66a8f5b19dc89c2ad30
Inou Ridder <inouridder@gmail.com>
2019-07-19 20:48:45 +0200

    FEAT: Add methods to extract values and keys by traversing the collection to Hash

--- lib/hash.rb
+++ lib/hash.rb
@@ -1,15 +1,13 @@
 class Hash
   class HashExtraction
-    attr_accessor :keys
-    attr_reader :hash
-    def initialize(hash)
-      @hash = hash
+    attr_accessor :keys, :values
+
+    def initialize
       @keys = []
+      @values = []
     end
 
     def extract_keys(hash = nil, prefix = '')
-      hash ||= self.hash
-      p hash
       hash.each do |key, value|
         if value.is_a? Hash
           extract_keys(value, "#{prefix}#{key}.")
@@ -19,9 +17,38 @@ class Hash
       end
       keys
     end
+
+    def extract_values(hash = nil)
+      hash.each do |_key, value|
+        if value.is_a? Hash
+          extract_values(value)
+        else
+          values << normalize(value)
+        end
+      end
+      values
+    end
+
+    def normalize(value)
+      value.is_a?(Array) ? value.join(',') : value.to_s
+    end
+  end
+
+  # def traverse
+  #   each do |key, value|
+  #     if value.is_a? Hash
+  #       traverse(self)
+  #     else
+  #       yield
+  #     end
+  #   end
+  # end
+
+  def extract_values
+    HashExtraction.new.extract_values(self)
   end
 
   def extract_keys
-    HashExtraction.new(self).extract_keys
+    HashExtraction.new.extract_keys(self)
   end
 end
--- spec/hash_spec.rb
+++ spec/hash_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe Hash do
   describe 'Hash#extract_keys' do
     let(:data) { JSON.parse(File.open(File.join(__dir__, 'examples/users.json')).read) }
 
-    it 'should return the headers of a simple hash' do
+    it 'should return the keys of a simple hash' do
       simple_hash = {
         'level' => '1',
         'live' => 'mentor'
@@ -20,6 +20,24 @@ RSpec.describe Hash do
     it 'should return all keys of a complex hash with nested denoated by dots' do
       actual = data.first.extract_keys
       expected = %w[id email tags profiles.facebook.id profiles.facebook.picture profiles.twitter.id profiles.twitter.picture]
+      expect(actual).to eq(expected)
+    end
+  end
+
+  describe 'Hash#extract_values' do
+    let(:data) { JSON.parse(File.open(File.join(__dir__, 'examples/users.json')).read) }
+    it 'should return the values of a simple hash' do
+      simple_hash = {
+        'level' => '1',
+        'live' => 'mentor'
+      }
+
+      expect(simple_hash.extract_values).to eq(simple_hash.values)
+    end
+
+    it 'should return all values of a nested hash' do
+      actual = data.first.extract_values
+      expected = %w[0 colleengriffith@quintity.com consectetur,quis 0 //fbcdn.com/a2244bc1-b10c-4d91-9ce8-184337c6b898.jpg 0 //twcdn.com/ad9e8cd3-3133-423e-8bbf-0602e4048c22.jpg]
       expect(actual).to eq(expected)
     end
   end
